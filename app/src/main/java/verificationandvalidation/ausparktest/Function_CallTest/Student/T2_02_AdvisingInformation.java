76f02051670d0681da595b0d45a79de677749a35
Jirasin Jirasakul <ball148862@hotmail.com>
2017-05-28 20:32:52 +0700

    T2

--- app/src/main/java/verificationandvalidation/ausparktest/Function_CallTest/Student/T2_02_AdvisingInformation.java
+++ app/src/main/java/verificationandvalidation/ausparktest/Function_CallTest/Student/T2_02_AdvisingInformation.java
@@ -1,8 +1,28 @@
 package verificationandvalidation.ausparktest.Function_CallTest.Student;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.appium.java_client.AppiumDriver;
+import verificationandvalidation.ausparktest.Provider.Functions;
+
 /**
  * Created by USER on 5/28/2017.
  */
 
 public class T2_02_AdvisingInformation {
+
+    Functions func = new Functions();
+
+    @Test
+    public void Test_T2_02_01(AppiumDriver driver) throws Exception {
+        func.SwipeToOpenNavigationBar(driver);
+        func.threadSleep_1();
+
+        func.getElementByID(driver, "edu.au.auspark:id/facebook_backgroundLoginButton").click();
+        func.threadSleep_2();
+
+        Assert.assertEquals(func.getElementByID(driver, "edu.au.auspark:id/regis_time_status").getText(),"Allowed");
+    }
+
 }
